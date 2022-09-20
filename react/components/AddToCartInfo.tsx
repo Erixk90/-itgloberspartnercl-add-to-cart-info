49047e3bd9dfb52c48b278c11f03c0501a771f93
Erixk90 <erickleonesreyes@gmail.com>
2022-09-20 08:47:46 -0500

    feat: finish the component and add styles

--- react/components/AddToCartInfo.tsx
+++ react/components/AddToCartInfo.tsx
@@ -1,54 +1,74 @@
 import React from 'react'
-import { useProduct } from "vtex.product-context";
-import { useOrderForm } from "vtex.order-manager/OrderForm";
-import ButtonGroup from './ButtonGroup';
+import { useProduct } from 'vtex.product-context';
+import { useOrderForm } from 'vtex.order-manager/OrderForm';
+//import ButtonGroup from './ButtonGroup';
+import { generateBlockClass } from '@vtex/css-handles'
+import styles from './styles.css'
 //import Totalizers from './Totalizers';
 //import ProductGroup from './ProductGroup';
 
-const AddToCartInfo = () => {
-    
-const productInfo = useProduct()
-const { orderForm:{
-    items,
-    totalizers
-} } = useOrderForm()
+const AddToCartInfo = ({ blockClass }: { blockClass: string }) => {
+    const container = generateBlockClass(styles.container, blockClass)
+    const container__item = generateBlockClass(styles.container__item, blockClass)
+    const container__image = generateBlockClass(styles.container__image, blockClass)
+    const container__description = generateBlockClass(styles.container__description, blockClass)
+    const container__summary = generateBlockClass(styles.container__summary, blockClass)
+    const summary__price = generateBlockClass(styles.summary__price, blockClass)
+    const container__checkout = generateBlockClass(styles.container__checkout, blockClass)
+    const button__checkout = generateBlockClass(styles.button__checkout, blockClass)
+    const button__checkout__cart = generateBlockClass(styles.button__checkout__cart, blockClass)
+    const productInfo = useProduct()
+    const { orderForm: {
+        items,
+        totalizers
+    } } = useOrderForm()
 
 
-console.log("este producto tiene esta info", productInfo)
-console.log("esta es mi información de la orden", /*items,*/ totalizers [0])
+    console.log("este producto tiene esta info", productInfo)
+    console.log("esta es mi información de la orden", /*items,*/ totalizers[0])
 
-return(
-<>
-   {/*<ProductGroup />   Listado de Productos */}
+    return (
+        <div className={container}>
+            {/*<ProductGroup />   Listado de Productos */}
 
-    {
-        items.map((item: any, index: number ) => {
-            console.log(item)
-        
-        return(
-            <div key={index}>
-                <div>
-                    <img src={item.imageUrls.at1x} />
+            {
+                items.map((item: any, index: number) => {
+                    console.log(item)
+
+                    return (
+                        <div key={index} className={container__item}>
+                            <div className={container__image}>
+                                <img src={item.imageUrls.at1x} />
+                            </div>
+                            <div className={container__description}>
+                                <p>{item.name}</p>
+                                <p> Id Producto:{item.id}</p>
+                                <p>${item.price / 100}</p>
+                                <p> Cant: {item.quantity}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+            {/* <Totalizers totalizers={totalizers=[0]}/>   Valor total */}
+
+            <div className={container__summary}>
+                <p> Tenemos {items.length} items en tu compra</p>
+                <p className={summary__price}> Total : ${totalizers[0]?.value / 100}</p>
+            </div>
+            {/*<ButtonGroup />  Acciones */}
+            <div>
+                <div className={container__checkout}>
+                    <a className={ button__checkout__cart} href='/'> CHECK OUT</a>
                 </div>
                 <div>
-                    <p>{item.name}</p>
-                    <p>{item.id}</p>
-                    <p>${item.price / 100}</p>
-                    <p> Cant: {item.quantity}</p>
+                    <button  className={ button__checkout}>CONTINÚA COMPRANDO</button>
+                    <a href='/'  className={ button__checkout__cart}> VER CARRITO</a>
                 </div>
             </div>
-        )
-    })
-    }
-   {/* <Totalizers totalizers={totalizers=[0]}/>   Valor total */}
-
-   <div>
-    <p> Tenemos {items.length} items en tu compra</p>
-    <p> Total : ${totalizers[0]?.value / 100 }</p>
-   </div>
-    <ButtonGroup /> {/* Acciones */}
-</>
+        </div>
 
 
-)}
+    )
+}
 export default AddToCartInfo
--- react/components/ButtonGroup.tsx
+++ react/components/ButtonGroup.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
+import { generateBlockClass } from '@vtex/css-handles'
+import styles from './styles.css'
 
-const ButtonGroup = () => {
+const ButtonGroup = ({ blockClass }:{ blockClass: string }) => {
+
+const container__checkout = generateBlockClass (styles.container__checkout, blockClass)
+
+
     return (
-        <>
-            <div>
+        <div>
+            <div className={container__checkout}>
         <a href='/'> CHECK OUT</a>
             </div>
             <div>
                 <button>CONTINÚA COMPRANDO</button>
                 <a href='/'> VER CARRITO</a>
             </div>
-        </>
+        </div>
     )
 }
 
